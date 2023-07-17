9a31a71ecf2ce2b9931d6c1480b9b5f7d97b5836
Dmitri.Chirov <dim.ch.it80@gmail.com>
2023-07-17 21:51:01 +0500

    new method _repr_ in class Phone

--- src/phone.py
+++ src/phone.py
@@ -1,5 +1,6 @@
 from src.item import Item
 
+
 class Phone(Item):
 
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
@@ -13,3 +14,6 @@ class Phone(Item):
         """
         super().__init__(name, price, quantity)
         self.number_of_sim = number_of_sim
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.sim_quantity})"
