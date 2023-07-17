103800fb79be0c9b385b81cc1587ea59a16e6954
Dmitri.Chirov <dim.ch.it80@gmail.com>
2023-07-17 22:23:48 +0500

    make attr number_of_sim private and add getter and setter for it

--- src/phone.py
+++ src/phone.py
@@ -13,7 +13,18 @@ class Phone(Item):
         :param sim_quantity: Количество слотов сим-карт.
         """
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self.__number_of_sim = number_of_sim
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value: int):
+        if isinstance(value, int) and (0 < value):
+            self.__number_of_sim = value
+        else:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
